e93dd016b61f93a5c08611d7c6be36cd02361825
rakibul114 <rakibulislam114@gmail.com>
2022-05-22 01:10:41 +0600

    installed react router for routing the components and tailwind css to style the components

---
+++ src/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>Explore more about us</h1>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/Home/Reviews.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Reviews = () => {
+    return (
+        <div>
+            <h1>Clients reviews</h1>
+        </div>
+    );
+};
+
+export default Reviews;
