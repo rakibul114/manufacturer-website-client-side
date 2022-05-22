7792c8d235dcdb533045413a09cf0f5ed3985ff9
rakibul114 <rakibulislam114@gmail.com>
2022-05-22 12:45:32 +0600

    json data and cards added at home

---
+++ src/Pages/Home/Tool.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Tool = ({ tool }) => {
+    const { _id, image, price, minimumOrder, available, name, description } = tool;
+
+    return (
+      <div>
+        <div class="card lg:w-96 md:w-80 w-80 bg-base-100 shadow-xl mx-auto">
+          <figure>
+            <img
+              src="https://api.lorem.space/image/shoes?w=400&h=225"
+              alt="Shoes"
+            />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default Tool;
--- src/Pages/Home/Tools.js
+++ src/Pages/Home/Tools.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Tool from './Tool';
 
 const Tools = () => {
+    const [tools, setTools] = useState([]);
+
+    useEffect(() => {
+        const url = 'tools.json';
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setTools(data));
+    }, []);
+
+
     return (
-        <div>
-            <h2>Our Tools</h2>
+      <div>
+        <h2 className="text-center text-4xl text-primary font-bold my-10">
+          OUR TOOLS
+        </h2>
+        <div className='container lg:mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-10 px-12'>
+          {tools.map((tool) => (
+            <Tool key={tool._id} tool={tool}></Tool>
+          ))}
         </div>
+      </div>
     );
 };
 
