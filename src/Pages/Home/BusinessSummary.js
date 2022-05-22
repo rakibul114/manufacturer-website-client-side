f7560d5d16272ba3706008d84d234f4795e4f28b
rakibul114 <rakibulislam114@gmail.com>
2022-05-22 16:21:59 +0600

    customer review section added on home page

--- src/Pages/Home/BusinessSummary.js
+++ src/Pages/Home/BusinessSummary.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import CountUp from "react-countup";
 
 const BusinessSummary = () => {
     return (
-        <div>
-            <h2>Business Summary</h2>
+      <div className="text-center">
+        <div className="grid grid-cols-3 bg-info my-12 p-10">
+          <div>
+            <h1 className="text-2xl">OVER</h1>
+            <div className="text-5xl">
+              <CountUp start={0} end={300} delay={0}>
+                {({ countUpRef }) => (
+                  <div>
+                    <span ref={countUpRef} />
+                  </div>
+                )}
+              </CountUp>
+            </div>
+            <h3 className="text-2xl">PRODUCTS</h3>
+          </div>
+          <div>
+            <h1 className="text-2xl">WE COME WITH</h1>
+            <div className="text-5xl">
+              <CountUp start={0} end={15} delay={0}>
+                {({ countUpRef }) => (
+                  <div>
+                    <span ref={countUpRef} />+
+                  </div>
+                )}
+              </CountUp>
+            </div>
+            <h3 className="text-2xl">YEARS OF EXPERIENCE</h3>
+          </div>
+          <div>
+            <h1 className="text-2xl">PRESENCE IN OVER</h1>
+            <div className="text-5xl">
+              <CountUp start={0} end={45} delay={0}>
+                {({ countUpRef }) => (
+                  <div>
+                    <span ref={countUpRef} />
+                  </div>
+                )}
+              </CountUp>
+            </div>
+            <h3 className="text-2xl">COUNTRIES</h3>
+          </div>
         </div>
+      </div>
     );
 };
 
--- src/Pages/Home/Reviews.js
+++ src/Pages/Home/Reviews.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import { Carousel } from "react-responsive-carousel";
+
+import client1 from '../../assets/reviews/client1.png';
+import client2 from '../../assets/reviews/client2.png';
+import client3 from '../../assets/reviews/client3.png';
+import client4 from '../../assets/reviews/client4.png';
+
 
 const Reviews = () => {
     return (
-        <div>
-            <h1>Clients reviews</h1>
+      <div>
+        <h1 className="text-center text-3xl">WHAT OUR CLIENTS SAY</h1>
+        <div className="mx-auto my-10">
+          <Carousel
+            autoPlay={true}
+            infiniteLoop={true}
+            showArrows={false}
+            showStatus={false}
+            showIndicators={false}
+            showThumbs={false}
+          >
+            <div>
+              <img style={{ width: "50px" }} src={client1} alt="" />
+            </div>
+            <div>
+              <img style={{ width: "50px" }} src={client2} alt="" />
+            </div>
+            <div>
+              <img style={{ width: "50px" }} src={client3} alt="" />
+            </div>
+            <div>
+              <img style={{ width: "50px" }} src={client4} alt="" />
+            </div>
+          </Carousel>
         </div>
+      </div>
     );
 };
 
