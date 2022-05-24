59dbd37058f36c8ffa4a5ad4805d3f8f7ca7e2ed
rakibul114 <rakibulislam114@gmail.com>
2022-05-24 19:59:43 +0600

    business summary updated

--- src/Pages/Home/BusinessSummary.js
+++ src/Pages/Home/BusinessSummary.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import CountUp from "react-countup";
+import toolsIcon from '../../assets/tools.png';
+import expertIcon from '../../assets/expert.png';
+import countriesIcon from '../../assets/countries.png';
 
 
 const BusinessSummary = () => {
     return (
       <div className="text-center my-24">
-        <div className="grid grid-cols-3 bg-info my-12 p-10">
-          <div>
-            <h1 className="text-2xl">OVER</h1>
+        <div className="grid grid-cols-3 bg-info my-12 p-12 items-center">
+          <div className="mx-auto">
+            <div>
+              <img
+                className="mx-auto"
+                style={{ height: "60px" }}
+                src={toolsIcon}
+                alt=""
+              />
+            </div>
+            <div>
+              <h1 className="text-xl">OVER</h1>
+            </div>
             <div className="text-5xl">
               <CountUp start={0} end={300} delay={0}>
                 {({ countUpRef }) => (
@@ -17,10 +30,23 @@ const BusinessSummary = () => {
                 )}
               </CountUp>
             </div>
-            <h3 className="text-2xl">PRODUCTS</h3>
+            <div>
+              <h3 className="text-xl">TOOLS</h3>
+            </div>
           </div>
-          <div>
-            <h1 className="text-2xl">WE COME WITH</h1>
+          {/*second */}
+          <div className="grid justify-center items-center">
+            <div>
+              <img
+                className="mx-auto"
+                style={{ height: "70px" }}
+                src={expertIcon}
+                alt=""
+              />
+            </div>
+            <div>
+              <h1 className="text-xl">WE COME WITH</h1>
+            </div>
             <div className="text-5xl">
               <CountUp start={0} end={15} delay={0}>
                 {({ countUpRef }) => (
@@ -30,10 +56,21 @@ const BusinessSummary = () => {
                 )}
               </CountUp>
             </div>
-            <h3 className="text-2xl">YEARS OF EXPERIENCE</h3>
+            <div>
+              <h3 className="text-xl">YEARS OF EXPERIENCE</h3>
+            </div>
           </div>
+          {/*third*/}
           <div>
-            <h1 className="text-2xl">PRESENCE IN OVER</h1>
+            <div>
+              <img
+                style={{ height: "70px" }}
+                className="mx-auto"
+                src={countriesIcon}
+                alt=""
+              />
+            </div>
+            <h1 className="text-xl">PRESENCE IN OVER</h1>
             <div className="text-5xl">
               <CountUp start={0} end={45} delay={0}>
                 {({ countUpRef }) => (
@@ -43,7 +80,7 @@ const BusinessSummary = () => {
                 )}
               </CountUp>
             </div>
-            <h3 className="text-2xl">COUNTRIES</h3>
+            <h3 className="text-xl">COUNTRIES</h3>
           </div>
         </div>
       </div>
